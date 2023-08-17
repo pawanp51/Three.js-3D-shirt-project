0b8e3c3c5c9cd75b4ae178904155237cb6d620c6
PawanP51 <pawanrp51@gmail.com>
2023-08-17 18:42:34 +0530

    add client, server files

---
+++ 3D_Shirt_Three.js/client/src/components/FilePicker.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import CustomButton from './CustomButton'
+
+const FilePicker = ({file, setFile, readFile}) => {
+  return (
+    <div className='filepicker-container'>
+      <div className='flex-1 flex-col'>
+        <input 
+          id='file-upload'
+          type='file'  
+          accept='image/*'
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label htmlFor='file-upload' className='filepicker-label'>
+          Upload File
+        </label>
+
+        <p className='mt-2 text-gray-500 text-xs truncate'>
+          {file === '' ? "No file selected" : file.name}
+        </p>
+      </div>
+
+      <div className='mt-4 flex flex-wrap gap-3'>
+      <CustomButton
+        type="outline"
+        title="Logo"
+        handleClick={() => readFile('logo')}
+        customSytles="text-xs"
+      />
+      <CustomButton
+        type="filled"
+        title="Full"
+        handleClick={() => readFile('full')}
+        customSytles="text-xs"
+      />
+      </div>
+    </div>
+  )
+}
+
+export default FilePicker
